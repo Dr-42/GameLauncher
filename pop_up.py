a4b802aa5c073ba57f9045227cd61f64f3cc69f7
Dr-42 <royspandan1@gmail.com>
2022-07-06 23:55:05 +0530

    Added remove game feature

--- pop_up.py
+++ pop_up.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
-import new_game
+import new_game, remove_game
 
 def open_add_game(win, data, lb):
    new_game.options(win, data, lb) 
    quit_pop()
 
-def open_remove_game():
-    pass
+def open_remove_game(win, data, lb):
+    remove_game.options(win, data, lb)
+    quit_pop()
 
 def quit_pop():
     pop.destroy()
@@ -21,7 +22,7 @@ def options(win, data, lb):
     p_head = tk.Label(frpop, text="Options")
     p_back = tk.Button(frpop, text="⏎", command=quit_pop)
     p_add_game = tk.Button(frpop, text="Add Game", command=lambda: open_add_game(win, data, lb))
-    p_remove_game = tk.Button(frpop, text="Remove Game", command=open_remove_game)
+    p_remove_game = tk.Button(frpop, text="Remove Game", command=lambda: open_remove_game(win, data, lb))
     p_head.grid(row=0, column=0)
     p_back.grid(row=0, column=1, sticky="e")
     p_add_game.grid(row=1, column=0, columnspan=2, sticky="ew")
---
+++ remove_game.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+from tkinter import ttk
+import json
+
+def delete_game(data, lb):
+    del data[game_name]
+    lb.delete(0, tk.END)
+    for game in data:
+        lb.insert(tk.END, game)
+
+    with open("/run/media/spandan/Projects/Python/GameLauncher/data.json", 'w') as json_file:
+        json.dump(data, json_file, indent=4, separators=(',',': '))
+
+    quit_pop()
+
+def quit_pop():
+    pop.destroy()
+
+def get_game_name(choice):
+    global game_name
+    game_name = choice 
+
+def options(win, data, lb):
+    global pop
+    pop = tk.Toplevel(win)
+    pop.title("Remove Game")
+
+    frpop = tk.Frame(pop)
+    p_head = tk.Label(frpop, text="Remove Game")
+    p_back = tk.Button(frpop, text="⏎", command=quit_pop)
+
+    menu = tk.StringVar()
+    menu.set("Select Game")
+    drop = tk.OptionMenu(frpop, menu, *data.keys(), command=get_game_name)
+    del_button = tk.Button(frpop, text="Delete Game", command=lambda: delete_game(data, lb))
+
+    p_head.grid(row=0, column=0, sticky="w")
+    p_back.grid(row=0, column=1, sticky="e")
+    drop.grid(row=1, column=0, columnspan=2)
+    del_button.grid(row=2, column=0, columnspan=2)
+
+    frpop.pack()
